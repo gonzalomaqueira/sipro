84c62e2341b4cbaa412c4523d9e41db74a4a7fe8
MAX-PC <MAX-PC@MAX-PC>
2018-10-16 20:46:09 -0300

    Comienzos de login

--- src/main/java/uy/edu/ude/sipro/SiproApplication.java
+++ src/main/java/uy/edu/ude/sipro/SiproApplication.java
@@ -1,11 +1,15 @@
 package uy.edu.ude.sipro;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan("uy.edu.ude")
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+
 public class SiproApplication {
 
 	public static void main(String[] args) {
--- src/main/java/uy/edu/ude/sipro/entidades/Elemento.java
+++ src/main/java/uy/edu/ude/sipro/entidades/Elemento.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -43,6 +45,7 @@ public class Elemento
 	
 	@NotNull
 	@Column(name= "TipoElemento")
+	@Enumerated(EnumType.STRING)
 	TipoElemento tipoElemento;
 	
 	@ManyToMany(fetch = FetchType.EAGER, cascade={CascadeType.ALL})
